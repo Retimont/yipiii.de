ed4e63b5a4e72c1c289a207176c624304c0091d3
mcberros <mcberros@gmail.com>
2015-10-09 10:45:16 +0200

    Removed not necesary code. #1

--- components/header.js
+++ components/header.js
@@ -1,29 +1,15 @@
 var Header = React.createClass({
   getInitialState: function() {
     return {
-      isScrolled: false,
-      isRoot: true
+      isScrolled: false
     };
   },
   componentDidMount: function() {
-    this.handleImageLogo();
     this.handleHeaderScroll();
     $(window).on('scroll', this.handleHeaderScroll);
   },
   componentWillUnmount: function() {
     $(window).off('scroll', this.handleHeaderScroll);
-    $(window).off('load', this.handleHeaderScroll);
-  },
-  handleImageLogo: function(){
-    var location = window.location.pathname;
-    // For yipiii.de
-    // this.setState({
-    //   isRoot: ((location === "/") || (location === "/index.html"))
-    // });
-
-    this.setState({
-      isRoot: ((location === "/it_frontend/carmen/yipiii.de/") || (location === "/it_frontend/carmen/yipiii.de/index.html"))
-    });
   },
   handleHeaderScroll: function() {
     var $window = $(window),
@@ -46,7 +32,7 @@ var Header = React.createClass({
         <div className={isScrolled ? "container small-head" : "container"}>
           <div className={isScrolled ? "for_con container" : "for_con"}>
             <div className="brand">
-              <a href="/"><img src={isRoot ? "img/yipiii_logo.png" : "../img/yipiii_logo.png"} alt="logo"/></a>&nbsp;&nbsp;&nbsp;&nbsp;Play&nbsp;&&nbsp;Win!
+              <a href="/"><img src="/img/yipiii_logo.png" alt="logo"/></a>&nbsp;&nbsp;&nbsp;&nbsp;Play&nbsp;&&nbsp;Win!
             </div>
             <div className="toggle_nav"><button className="cmn-toggle-switch cmn-toggle-switch__htx"><span></span></button></div>
             <nav className="navigation">
